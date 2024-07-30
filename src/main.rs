ebdb274234543592c40e55ca12b4244d65871d7f
quantumshiro <hiro.1nakanishi@gmail.com>
2024-07-30 16:24:48 +0900

    add: actix web lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+use polytorus::app::route::index;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
