a58c7f2f634674d2d8ce9360d86e869b35e34753
quantumshiro <hiro.1nakanishi@gmail.com>
2024-08-01 18:27:58 +0900

    add: mine adjust difficulty

--- src/blockchain/block.rs
+++ src/blockchain/block.rs
@@ -2,7 +2,7 @@ use std::fmt;
 use std::time::SystemTime;
 use sha2::{Digest, Sha256};
 use serde::{Serialize, Deserialize};
-use super::config::DIFFICULTY;
+use super::config::{DIFFICULTY, MINE_RATE};
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Block {
@@ -11,21 +11,23 @@ pub struct Block {
     pub hash: String,
     pub nonce: u64,
     pub data: String,
+    pub difficulty: u32,
 }
 
 impl Block {
-    pub fn new(timestamp: u64, last_hash: String, hash: String, nonce: u64, data: String) -> Block {
+    pub fn new(timestamp: u64, last_hash: String, hash: String, nonce: u64, data: String, difficulty: u32) -> Block {
         Block {
             timestamp,
             last_hash,
             hash,
             nonce,
             data,
+            difficulty,
         }
     }
 
     pub fn genesis() -> Block {
-        Block::new(0, "genesis_last_hash".to_string(), "genesis_hash".to_string(), 0,"genesis_data".to_string())
+        Block::new(0, "genesis_last_hash".to_string(), "genesis_hash".to_string(), 0,"genesis_data".to_string(), DIFFICULTY)
     }
 
     pub fn mine_block(last_block: &Block, data: String) -> Block {
@@ -35,30 +37,44 @@ impl Block {
             .unwrap()
             .as_secs();
         let mut nonce = 0;
+        let mut difficulty = last_block.difficulty;
     
-        let mut hash = Block::hash(timestamp, last_hash.clone(), nonce, data.clone());
-    
-        while hash.chars().take(DIFFICULTY.try_into().unwrap()).all(|c| c == '0') == false {
+        let mut hash = Block::hash(timestamp, last_hash.clone(), nonce, data.clone(), difficulty);
+        while !hash.starts_with(&"0".repeat(difficulty as usize)) {
             nonce += 1;
             timestamp = SystemTime::now()
                 .duration_since(SystemTime::UNIX_EPOCH)
                 .unwrap()
                 .as_secs();
-            hash = Block::hash(timestamp, last_hash.clone(), nonce, data.clone());
+            difficulty = Block::adjust_difficulty(last_block, timestamp);
+            hash = Block::hash(timestamp, last_hash.clone(), nonce, data.clone(), difficulty);
         }
     
-        Block::new(timestamp, last_hash, hash, nonce, data)
+        Block::new(timestamp, last_hash, hash, nonce, data, last_block.difficulty)
     }
 
-    pub fn hash(timestamp: u64, last_hash: String, nonce: u64, data: String) -> String {
-        let input = format!("{}{}{}{}", timestamp, last_hash, nonce, data);
+    pub fn hash(timestamp: u64, last_hash: String, nonce: u64, data: String, difficulty: u32) -> String {
+        let input = format!("{}{}{}{}{}", timestamp, last_hash, nonce, data, difficulty);
         let mut hasher = Sha256::new();
         hasher.update(input);
         format!("{:x}", hasher.finalize())
     }
 
     pub fn hash_block(block: &Block) -> String {
-        Block::hash(block.timestamp, block.last_hash.clone(),  block.nonce, block.data.clone())
+        Block::hash(block.timestamp, block.last_hash.clone(),  block.nonce, block.data.clone(), block.difficulty)
+    }
+
+    pub fn adjust_difficulty(last_block: &Block, current_time: u64) -> u32 {
+        let difficulty = last_block.difficulty;
+        if difficulty < 1 {
+            return 1;
+        }
+
+        if (current_time - last_block.timestamp) < MINE_RATE {
+            difficulty + 1
+        } else {
+            difficulty - 1
+        }
     }
 }
 
@@ -66,8 +82,8 @@ impl fmt::Display for Block {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "Block - Timestamp: {}, Last Hash: {}, Hash: {}, Data: {}",
-            self.timestamp, self.last_hash, self.hash, self.data
+            "Block - Timestamp: {}, Last Hash: {}, Hash: {},Nonce: {}, Data: {}, Difficulty: {}",
+            self.timestamp, self.last_hash, self.hash, self.nonce, self.data, self.difficulty
         )
     }
 }
@@ -78,22 +94,23 @@ mod tests {
 
     #[test]
     fn block_new() {
-        let block = Block::new(0, "foo".to_string(), "bar".to_string(), 0, "baz".to_string());
+        let block = Block::new(0, "foo".to_string(), "bar".to_string(), 0, "baz".to_string(), 0);
 
         assert_eq!(block.timestamp, 0);
         assert_eq!(block.last_hash, "foo".to_string());
         assert_eq!(block.hash, "bar".to_string());
         assert_eq!(block.nonce, 0);
         assert_eq!(block.data, "baz".to_string());
+        assert_eq!(block.difficulty, 0);
     }
 
     #[test]
     fn block_display() {
-        let block = Block::new(0, "foo".to_string(), "bar".to_string(), 0, "baz".to_string());
+        let block = Block::new(0, "foo".to_string(), "bar".to_string(), 0, "baz".to_string(), 0);
 
         assert_eq!(
             format!("{}", block),
-            "Block - Timestamp: 0, Last Hash: foo, Hash: bar, Data: baz"
+            "Block - Timestamp: 0, Last Hash: foo, Hash: bar,Nonce: 0, Data: baz, Difficulty: 0"
         );
     }
 
@@ -115,18 +132,6 @@ mod tests {
 
         assert_eq!(mined_block.last_hash, last_block.hash);
         assert_eq!(mined_block.data, data);
-    }
-
-    #[test]
-    fn block_hash() {
-        let timestamp = 0;
-        let last_hash = "foo".to_string();
-        let nonce = 0;
-        let data = "baz".to_string();
-        let hash = Block::hash(timestamp, last_hash.clone(), nonce, data.clone());
-        let expected_hash = Block::hash(timestamp, last_hash.clone(), nonce, data.clone());
-
-        assert_eq!(hash, expected_hash);
     }
 
 }
