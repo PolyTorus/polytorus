cc064eea9efbdcee8b49d3f92ddeabc05cab1692
quantumshiro <hiro.1nakanishi@gmail.com>
2024-08-03 00:17:41 +0900

    add: field element eq

--- src/cryptography/field_element.rs
+++ src/cryptography/field_element.rs
@@ -25,6 +25,7 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use primitive_types::U256;
 
     #[test]
     fn field_element_new() {
@@ -39,5 +40,15 @@ mod tests {
         let field_element = FieldElement::new(5, 5);
 
         assert_eq!(field_element, Err("Num 5 not in field range 0 to 5".to_string()));
+    }
+
+    #[test]
+    fn eq() {
+        let a = FieldElement::new(U256::from(2), U256::from(3));
+        let b = FieldElement::new(U256::from(2), U256::from(3));
+        let c = FieldElement::new(U256::from(1), U256::from(3));
+
+        assert_eq!(a, b);
+        assert_ne!(a, c);
     }
 }
