47bd11c06b07af89e915c8439d41ede606dda6c2
quantumshiro <hiro.1nakanishi@gmail.com>
2024-08-08 14:04:14 +0900

    add: wallet ecdsa sign

--- src/wallet/wallets.rs
+++ src/wallet/wallets.rs
@@ -1,8 +1,8 @@
 use std::fmt;
 use secp256k1::rand::rngs::OsRng;
-use secp256k1::Secp256k1;
-use uuid::Uuid;
+use secp256k1::{Secp256k1, Message};
 use crate::blockchain::config::INITIAL_BALANCE;
+use secp256k1::hashes::{sha256, Hash};
 
 pub struct Wallet {
     pub balance: u64,
@@ -21,6 +21,14 @@ impl Wallet {
             public_key: public_key,
         }
     }
+
+    fn sign(message: String, secret_key: &secp256k1::SecretKey) -> secp256k1::ecdsa::Signature {
+        let secp = Secp256k1::new();
+        let digest = sha256::Hash::hash(message.as_bytes());
+        let message = Message::from_digest(digest.to_byte_array());
+        let sig = secp.sign_ecdsa(&message, &secret_key);
+        sig
+    }
 }
 
 impl fmt::Display for Wallet {
@@ -38,5 +46,14 @@ mod tests {
     fn test_wallet_new() {
         let wallet = Wallet::new();
         println!("{}", wallet);
+    }
+
+    #[test]
+    fn test_wallet_sign() {
+        let wallet = Wallet::new();
+        let message = "Hello, world!".to_string();
+        let signature = Wallet::sign(message, &wallet.keypair.secret_key());
+        println!("wallet keypair secret_key: {:?}", wallet.keypair.secret_key());
+        println!("signature: {:?}", signature);
     }
 }
