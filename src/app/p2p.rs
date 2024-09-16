1de9dd2f712d9bb74a3130172aef4a7a03c95e4e
quantumshiro <hiro.1nakanishi@gmail.com>
2024-09-17 03:09:51 +0900

    fix: try use tokio

--- src/app/p2p.rs
+++ src/app/p2p.rs
@@ -1,113 +1,111 @@
-use actix::prelude::*;
-use actix_web::{web, HttpRequest, Responder};
-use actix_web_actors::ws;
 use crate::blockchain::chain::Chain;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use futures::{SinkExt, StreamExt};
+use tokio::net::TcpListener;
+use tokio::sync::Mutex;
+use tokio::net::TcpStream;
+use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
+use serde_json::json;
+
+type WsStream = WebSocketStream<MaybeTlsStream<TcpStream>>;
 
 #[derive(Debug, Clone)]
 pub struct P2p {
     chain: Arc<Mutex<Chain>>,
-    sockets: Vec<actix::Addr<WebSocketSession>>,
-}
-
-#[derive(Debug, Clone)]
-pub struct WebSocketSession {
-    server: Arc<Mutex<P2p>>
-}
-
-impl Actor for WebSocketSession {
-    type Context = ws::WebsocketContext<Self>;
-}
-
-impl WebSocketSession {
-    pub fn message_handler(&self, text: String, ctx: &mut ws::WebsocketContext<Self>) {
-        match serde_json::from_str::<Chain>(&text) {
-            Ok(received_chain) => {
-                println!("Received chain: {:?}", received_chain);
-                let server = self.server.lock().unwrap();
-                server.chain.lock().unwrap().replace_chain(&received_chain);
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
-        }
-    }
-}
-
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocketSession {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        match msg {
-            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => println!("Received: {}", text),
-            _ => (),
-        }
-    }
-}
-
-#[derive(Message)]
-#[rtype(result = "()")]
-struct SendChain(String);
-
-impl Handler<SendChain> for WebSocketSession {
-    type Result = ();
-
-    fn handle(&mut self, msg: SendChain, ctx: &mut Self::Context) {
-        ctx.text(msg.0);
-    }
+    sockets: Arc<Mutex<Vec<Arc<Mutex<WsStream>>>>>,
 }
 
 impl P2p {
     pub fn new(chain: Chain) -> Self {
         P2p {
             chain: Arc::new(Mutex::new(chain)),
-            sockets: Vec::new(),
+            sockets: Arc::new(Mutex::new(Vec::new()))        
         }
     }
 
-    pub fn connect_socket(&mut self, addr: actix::Addr<WebSocketSession>) {
-        let addr_clone = addr.clone();
-        self.sockets.push(addr);
-        println!("Connected sockets: {}", self.sockets.len());
+    pub async fn listen(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let p2p_port = std::env::var("P2P_PORT").unwrap_or_else(|_| "5001".to_string());
+        let addr = format!("127.0.0.1:{}", p2p_port);
+        let listener = TcpListener::bind(&addr).await?;
+        println!("Listening on: {}", addr);
+        
+        while let Ok((stream, _)) = listener.accept().await {
+            let ws_stream = tokio_tungstenite::accept_async(tokio_tungstenite::MaybeTlsStream::Plain(stream)).await.expect("Failed to accept");
+            self.connect_socket(ws_stream).await;
+        }
 
-        let chain = self.chain.lock().unwrap().clone();
-        let chain_json = serde_json::to_string(&chain).unwrap();
-
-        addr_clone.do_send(SendChain(chain_json));
+        Ok(())
     }
 
-    pub async fn connect_peers(&self) {
+    pub async fn connect_peers(&self) -> Result<(), Box<dyn std::error::Error>>{
         let peers = std::env::var("PEERS").unwrap_or_default();
 
         for peer in peers.split(",") {
             if !peer.is_empty() {
-                // Connect to peer
-                println!("Connecting to peer: {}", peer);
+                let (ws_stream, _) = connect_async(peer).await?;
+                self.connect_socket(ws_stream).await;
+            }
+        }
+        Ok(())
+    }
+
+    pub async fn connect_socket(&self, ws_stream: WsStream) {
+        let chain = self.chain.clone();
+        let sockets = self.sockets.clone();
+        let ws_stream = Arc::new(Mutex::new(ws_stream));
+
+        {
+            let mut sockets = sockets.lock().await;
+            sockets.push(ws_stream.clone());
+            println!("Socket connected");
+        }
+
+        let chain_json = {
+            let chain = chain.lock().await;
+            json!(&*chain).to_string()
+        };
+        
+        {
+            let mut ws_stream = ws_stream.lock().await;
+            ws_stream.send(tokio_tungstenite::tungstenite::Message::Text(chain_json)).await.unwrap();
+        }
+
+        let chain_for_message = chain.clone();
+        let ws_stream_clone = ws_stream.clone();
+        tokio::spawn(async move {
+            loop {
+                let msg = {
+                    let mut ws_stream = ws_stream_clone.lock().await;
+                    ws_stream.next().await
+                };
+
+                match msg {
+                    Some(Ok(tokio_tungstenite::tungstenite::Message::Text(text))) => {
+                        if let Ok(received_chain) = serde_json::from_str::<Chain>(&text) {
+                            println!("Received chain: {:?}", received_chain);
+                            let mut current_chain = chain_for_message.lock().await;
+                            current_chain.replace_chain(&received_chain);
+                        }
+                    }
+                    None => break,
+                    _ => {}
+                }
+            }
+        });
+    }
+
+    pub async fn broadcast_chain(&self) {
+        let chain_json = {
+            let chain = self.chain.lock().await;
+            json!(&*chain).to_string()
+        };
+        
+        let sockets = self.sockets.lock().await;
+        for socket in sockets.iter() {
+            let mut socket = socket.lock().await;
+            if let Err(e) = socket.send(tokio_tungstenite::tungstenite::Message::Text(chain_json.clone())).await {
+                eprintln!("Failed to send message: {}", e);
             }
         }
     }
-
-    pub fn broadcast_chain(&self) {
-        let chain = self.chain.lock().unwrap().clone();
-        let chain_json = serde_json::to_string(&chain).unwrap();
-
-        for socket in &self.sockets {
-            socket.do_send(SendChain(chain_json.clone()));
-        }
-    }
-}
-
-pub async fn websocket_route(
-    req: actix_web::HttpRequest,
-    stream: web::Payload,
-    server: web::Data<Arc<Mutex<P2p>>>,
-) -> Result<actix_web::HttpResponse, actix_web::Error> {
-    let server_clone = server.get_ref().clone();
-
-    ws::start(
-        WebSocketSession {
-            server: server_clone,
-        },
-        &req,
-        stream,
-    )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use actix_web::{App, HttpServer, web};
-use polytorus::app::p2p::{websocket_route, P2p};
+use polytorus::app::p2p::P2p;
 use polytorus::app::route::index;
 use polytorus::app::show_block::block;
 use polytorus::blockchain::chain::Chain;
 use polytorus::app::mine::mine;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -13,18 +14,24 @@ async fn main() -> std::io::Result<()> {
 
     let p2p_port = std::env::var("P2P_PORT").unwrap_or_else(|_| "8081".to_string());
     let server_clone = server.clone();
+    let _ = server.lock().await.connect_peers().await;
 
-    server.lock().unwrap().connect_peers().await;
+    let server_clone_for_spawn = server_clone.clone();
+    tokio::spawn(async move {
+        if let Err(e) = server_clone_for_spawn.lock().await.connect_peers().await {
+            eprintln!("Error connecting to peers: {}", e);
+        }
+    });
 
     HttpServer::new(move || {
+        let server_clone = server_clone.clone();
         App::new()
             .app_data(web::Data::new(blockchain.clone()))
-            .app_data(web::Data::new(server_clone.clone()))
+            .app_data(web::Data::new(server_clone))
             .service(index)
             .service(block)
             .service(mine)
             .service(web::redirect("/mine", "/block"))
-            .route("/ws/", web::get().to(websocket_route))
     })
     .bind(format!("127.0.0.1:{}", p2p_port))?
     .run()
