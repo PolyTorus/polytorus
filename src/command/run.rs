f738893c3d37d622c2fd6f06288e08ea840849db
Placeless <mikoprpr@hotmail.com>
2025-03-05 15:06:33 +0900

    clap::SubCommand で定義する

--- src/command/run.rs
+++ src/command/run.rs
@@ -1,11 +1,12 @@
 use crate::command::term;
-use clap::App;
+use clap::{App, SubCommand};
 use crate::Result;
-
 
 pub fn handle_term() -> Result<()> {
     let matches = App::new("run_tui")
+        .subcommand(SubCommand::with_name("run_tui")) // サブコマンドの定義
         .get_matches();
+
     if let Some(ref _matches) = matches.subcommand_matches("run_tui") {
         term::run_tui()?;
     } else {
