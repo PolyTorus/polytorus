6128ea09cb7faf215669f91e671cc9df56cd2aa7
teru12012000 <sion2769@gmail.com>
2025-03-12 23:31:47 +0900

    feat: create web api

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use env_logger::Env;
 use polytorus::command::cli::Cli;
+use actix_web;
 
-fn main() {
+#[actix_web::main]
+async fn main() {
     env_logger::from_env(Env::default().default_filter_or("warning")).init();
 
     let mut cli = Cli::new();
-    if let Err(e) = cli.run() {
+    if let Err(e) = cli.run().await {
         println!("Error: {}", e);
     }
 }
