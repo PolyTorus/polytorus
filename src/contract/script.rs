15a8a0d0c44e110217399bac5f9b289c7d118f29
quantumshiro <hiro.1nakanishi@gmail.com>
2025-04-18 00:28:39 +0900

    fix: reorganize imports and define ValueType and Value structures in script module

--- src/contract/script.rs
+++ src/contract/script.rs
@@ -1,6 +1,6 @@
 use bincode::de;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::{collections::HashMap, hash::Hash};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum ScriptType {
@@ -16,3 +16,17 @@ pub struct Script {
     pub hash: String,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum ValueType {
+    Integer(i64),
+    Boolean(bool),
+    String(String),
+    Bytes(Vec<u8>),
+    List(Vec<Value>),
+    Map(HashMap<String, Value>),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Value {
+    pub value_type: ValueType,
+}
