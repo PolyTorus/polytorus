348a58cc17cd246ab9d5ba7d0530d267e2da1238
quantumshiro <hiro.1nakanishi@gmail.com>
2025-04-27 03:22:10 +0900

    add: the ability to convert from code, display and infer from public key size

--- src/crypto/types.rs
+++ src/crypto/types.rs
@@ -1,9 +1,41 @@
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum EncryptionType {
     ECDSA,
     FNDSA,
 }
 
-pub enum DecryptionType {
-    ECDSA,
-    FNDSA,
+impl EncryptionType {
+    pub fn from_code(code: &str) -> Option<EncryptionType> {
+        match code {
+            "ECDSA" => Some(EncryptionType::ECDSA),
+            "FNDSA" => Some(EncryptionType::FNDSA),
+            _ => None,
+        }
+    }
+
+    pub fn to_code(&self) -> &str {
+            match self {
+                EncryptionType::ECDSA => "ECDSA",
+                EncryptionType::FNDSA => "FNDSA",
+        }
+    }
+
+    pub fn guess_from_pubkey_size(size: usize) -> Option<EncryptionType> {
+        match size {
+            65 => Some(EncryptionType::ECDSA),
+            512 => Some(EncryptionType::FNDSA),
+            _ => None,
+        }
+        
+    }
+}
+
+impl fmt::Display for EncryptionType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_code())
+    }
+    
 }
