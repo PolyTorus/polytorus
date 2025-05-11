59a2a766fea8ea5e6a231b132c51d9f69174730b
darkcat666 <holycat666@gmail.com>
2025-05-11 18:47:01 +0900

    I fix feature/createblockchain again

--- src/webserver/remotesend.rs
+++ src/webserver/remotesend.rs
@@ -14,13 +14,18 @@ struct RemoteSendParams {
 #[post("/remote_send")]
 pub async fn remote_send(req: web::Json<RemoteSendParams>) -> impl Responder {
     let req_data = req.into_inner();
-    let from = &req_data.from;
-    let to = &req_data.to;
-    let amount = req_data.amount;
-    let node = &req_data.node;
-    let mine = req_data.mine;
-    match cmd_remote_send(from, to, amount, node, mine) {
-        Ok(()) => HttpResponse::Ok().body("Complete remote send"),
-        Err(err) => HttpResponse::BadGateway().body(err.to_string()),
-    }
+    let from = req_data.from.clone();
+    let to = req_data.to.clone();
+    let amount = req_data.amount.clone();
+    let node = req_data.node.clone();
+    let mine = req_data.mine.clone();
+    tokio::task::spawn_blocking(move || {
+        if let Err(e) = cmd_remote_send(&from, &to, amount, &node, mine) {
+            eprintln!("Miner failed: {}", e);
+        } else {
+            println!("Remote_send success");
+        }
+    });
+
+    HttpResponse::Accepted().body("Remote_send success")
 }
